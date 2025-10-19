8f5c5bb759c4468b860e8f0261ca77168e08a77f
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2025-10-19 10:26:58 +0530

    Create ExploreDataTypes.py
    Explores the primitive data types in Python

---
+++ ExploreDataTypes.py
@@ -0,0 +1,11 @@
+# Creating variables of different data types
+integer_value = 42           # int
+float_value = 3.14           # float
+string_value = "Hello"       # str
+boolean_value = True         # bool
+
+# Printing the values and their data types
+print("Value:", integer_value, "Type:", type(integer_value))
+print("Value:", float_value, "Type:", type(float_value))
+print("Value:", string_value, "Type:", type(string_value))
+print("Value:", boolean_value, "Type:", type(boolean_value))
